b6efd8c5c683e54963a037fb3288c2271df21e1a
Blake Becerra <bab72@duke.edu>
2017-04-02 12:23:10 -0400

    Starting the newsfeed

---
+++ FacebookLogin/SocialLogin/FeedTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FeedTableViewCell.swift
+//  SocialLogin
+//
+//  Created by Blake Becerra on 3/30/17.
+//  Copyright © 2017 Cathy Chi. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var feedButton: UIButton!
+    @IBOutlet weak var feedImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
---
+++ FacebookLogin/SocialLogin/NewsfeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsfeedTableViewCell.swift
+//  SocialLogin
+//
+//  Created by Blake Becerra on 3/30/17.
+//  Copyright © 2017 Cathy Chi. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
